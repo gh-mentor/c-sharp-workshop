2981396eae70097838f1a7f85d01effb0498bf55
Andrew C. Scoppa <andrew.scoppa@gmail.com>
2025-08-26 09:32:58 -0400

    added review

--- ex5-rest/README.md
+++ ex5-rest/README.md
@@ -8,5 +8,7 @@ The projects were created using the Visual Studio templates for .NET 8.0. The 'p
 Given the current baseline, the following tasks are required to complete the project:
 - Add additional API endpoints to the Web API project to support CRUD operations for the Products, Categories, and Suppliers tables.
 - Implement error handling and logging in the Web API project to ensure that any errors are properly logged and handled.
-- Add unit tests for the new API endpoints in the Test project to ensure that they are working as expected.
+- Add unit tests for the new API endpoints in the Test project to ensure that they are working as expected. 
+- Design a client-side app that calls into the 'CoreWebApi' project. The app will use TypeScript and Angular.io.
+
 - Optional: Implement authentication and authorization in the Web API project to secure the endpoints and restrict access to authorized users only.
---
+++ review/PayrollLibrary/Employee.cs
@@ -0,0 +1,63 @@
+﻿/*
+This file contains the abstract class Employee. This class is the base for the HourlyEmployee and SalariedEmployee classes. It contains the properties and methods that are common to both classes.
+*/
+
+/*
+TODO
+Add a protected virtual method 'validateEmail' to the Employee classthat will use regular expression syntax to validate an employee email a return false if the email is not valid.
+Add a public abstract bool 'setEmail' method to the Employee class.  The method will call 'validateEmail' in the base class
+Implement 'setEmail' in Hourly Employee and Salaried Employee derived class.
+*/quit
+
+
+namespace PayrollLibrary
+{
+
+  /*
+  Create a public class 'Employee'.
+  Details:
+  - The class is abstract.
+  - The class has the following nullable public properties:
+    - Id (int, read-only)
+    - ReportsTo (int)
+    - Name (string)
+    - Email (string)
+    - Mobile (string)
+    - DepartmentId (int)
+  - The class has a public c'tor that accepts all the properties as parameters.
+  - The class has the following public properties:
+    - Payment (double) - get-only, abstract
+  - The class has the following public methods:
+    - EmployeeDetails (string) - get-only, virtual
+  */
+
+  public abstract class Employee
+  {
+    public int? Id { get; }
+    public int? ReportsTo { get; set; }
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public string? Mobile { get; set; }
+    public int? DepartmentId { get; set; }
+
+    public Employee(int? id, int? reportsTo, string? name, string? email, string? mobile, int? departmentId)
+    {
+      Id = id;
+      ReportsTo = reportsTo;
+      Name = name;
+      Email = email;
+      Mobile = mobile;
+      DepartmentId = departmentId;
+    }
+
+    public abstract double Payment { get; }
+    public virtual string EmployeeDetails
+    {
+      get
+      {
+        return $"Id: {Id}, Name: {Name}, Email: {Email}, Mobile: {Mobile}, DepartmentId: {DepartmentId}";
+      }
+    }
+  }
+
+}
